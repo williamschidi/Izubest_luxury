12cf0225de6e726838fdc3915f1a27e4cf8e684a
williamchidi <williamchidi1@gmail.com>
2025-10-18 10:14:13 +0100

    updated

--- src/components/userComponents/Hero.jsx
+++ src/components/userComponents/Hero.jsx
@@ -14,7 +14,7 @@ function Hero() {
           <h1 className="text-3xl md:text-4xl  font-bold mb-2 md:mb-4 text-white  font-calligraphy tracking-tighter">
             Elevate Your Style
           </h1>
-          <p className="text-lg max-w-[24rem] text-center md:text-left mlg:max-w-full mb-4 md:mb-6 text-white font-semibold md:font-medium font-poppins px-2">
+          <p className="text-sm md:text-lg max-w-[24rem] text-center md:text-left mlg:max-w-full mb-4 md:mb-6 text-white font-semibold md:font-medium font-poppins px-2">
             Discover unique, modern, and culturally inspired
             designs tailored for you.
           </p>
